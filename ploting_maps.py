0f0a431cd66bf55ba94499e6201b0e93366bf98a
debjyoti <debjyoti.chowdhury@heritageit.edu>
2022-12-07 11:16:38 +0530

    initial commit

--- ploting_maps.py
+++ ploting_maps.py
@@ -1,39 +1,42 @@
 import plotly.express as px
 import pandas as pd
 import requests
+import numpy as np
 
 maps_api_token = "pk.eyJ1IjoiZGVianlvdGljIiwiYSI6ImNsYmFpY3p6OTAwb3IzcW1vMmRkcW15aTMifQ.STVwLHWK7VjBcXA4hvdi6A"
 cloud_api = "C4EUQWKMRDFBT1ZX1S14PG"
 
-pod_url = f"http://consentiuminc.online/feeds?receive_key={cloud_api}&all=false"
+pod_url = f"http://consentiuminc.online/feeds?receive_key={cloud_api}&all=true"
 data = requests.get(pod_url).json()['feeds'][0]
 
 sensor_data = data['sensor_data']
 sensor_info = data['sensor_info']
 updated_at = data['updated_at']
 
-latitude = [i[0] for i in sensor_data]
-longitude = [i[1] for i in sensor_data]
-velocity = [i[2] for i in sensor_data]
-road_quality = [i[3] for i in sensor_data]
+latitude = np.array([i[0] for i in sensor_data])
+longitude = np.array([i[1] for i in sensor_data])
+velocity = np.array([i[2] for i in sensor_data])
+road_quality = np.array([i[3] for i in sensor_data])
 
-print(velocity)
-time = [i.split(" ")[1] for i in updated_at]
+print("Lat: ", latitude[latitude != 0])
+print("Long: ", longitude[latitude != 0])
+print("Velocity: ", velocity[latitude != 0])
+print("Quality: ", road_quality[latitude != 0])
 
-gps_df = pd.DataFrame(list(zip(latitude, longitude, velocity, road_quality, time)), columns=['latitude', 'longitude',
-                                                                                             'velocity', 'road_quality',
-                                                                                             'time'])
-
-fig = px.scatter_mapbox(gps_df, lat="latitude", lon="longitude", color='velocity',
-                        color_continuous_scale=["black", "purple", "red"], size_max=30, zoom=12.5,
-                        height=600, width=1000, title='Road Quality')
-
-fig.update_layout(font_size=16, title={'xanchor': 'center', 'yanchor': 'top', 'y': 0.9, 'x': 0.5, },
-                  title_font_size=24, mapbox_accesstoken=maps_api_token,
-                  mapbox_style="mapbox://styles/strym/ckhd00st61aum19noz9h8y8kw")
-
-fig.update_traces(marker=dict(size=6))
-
-# fig.write_image('gps.png')
-# fig.write_html('gps.html')
-fig.show()
+# gps_df = pd.DataFrame(list(zip(latitude, longitude, velocity, road_quality, time)), columns=['latitude', 'longitude',
+#                                                                                              'velocity', 'road_quality',
+#                           ])
+#
+# fig = px.scatter_mapbox(gps_df, lat="latitude", lon="longitude", color='velocity',
+#                         color_continuous_scale=["black", "purple", "red"], size_max=30, zoom=12.5,
+#                         height=1000, width=1000, title='Road Quality')
+#
+# fig.update_layout(font_size=16, title={'xanchor': 'center', 'yanchor': 'top', 'y': 0.9, 'x': 0.5, },
+#                   title_font_size=24, mapbox_accesstoken=maps_api_token,
+#                   mapbox_style="mapbox://styles/strym/ckhd00st61aum19noz9h8y8kw")
+#
+# fig.update_traces(marker=dict(size=6))
+#
+# # fig.write_image('gps.png')
+# # fig.write_html('gps.html')
+# fig.show()
